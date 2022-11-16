da5c754cc87b6f26108b97ca7352fd4b1cd0dd11
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-11-16 14:27:57 -0500

    Simple text classification script

---
+++ scripts/text_classification.py
@@ -0,0 +1,232 @@
+# coding=utf-8
+# Copyright 2021 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Finetuning a 🤗 Transformers model for sequence classification on GLUE."""
+import argparse
+import logging
+import time
+
+import datasets
+import torch
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+import evaluate
+import transformers
+from accelerate import Accelerator
+from accelerate.logging import get_logger
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    default_data_collator,
+    get_scheduler,
+)
+
+
+logger = get_logger(__name__)
+
+task_to_keys = {
+    "cola": ("sentence", None),
+    "mnli": ("premise", "hypothesis"),
+    "mrpc": ("sentence1", "sentence2"),
+    "qnli": ("question", "sentence"),
+    "qqp": ("question1", "question2"),
+    "rte": ("sentence1", "sentence2"),
+    "sst2": ("sentence", None),
+    "stsb": ("sentence1", "sentence2"),
+    "wnli": ("sentence1", "sentence2"),
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Finetune a transformers model on a text classification task")
+    parser.add_argument(
+        "--task_name",
+        type=str,
+        default=None,
+        help="The name of the glue task to train on.",
+        choices=list(task_to_keys.keys()),
+    )
+    parser.add_argument(
+        "--max_length",
+        type=int,
+        default=128,
+        help=(
+            "The maximum total input sequence length after tokenization. Sequences longer than this will be truncated,"
+            " sequences shorter will be padded unless `--dynamic_lengh` is passed."
+        ),
+    )
+    parser.add_argument(
+        "--dynamic_length",
+        action="store_true",
+        help="If passed, pad all samples to `max_length`. Otherwise, dynamic padding is used.",
+    )
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+        default="bert-base-cased",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        help="Batch size (per device) for the dataloaders.",
+    )
+    parser.add_argument(
+        "--num_epochs",
+        type=int,
+        default=3,
+        help="Number of training epochs.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    accelerator = Accelerator()
+
+    # Make one log on every process with the configuration for debugging.
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger.info(accelerator.state, main_process_only=False)
+    if accelerator.is_local_main_process:
+        datasets.utils.logging.set_verbosity_warning()
+        transformers.utils.logging.set_verbosity_info()
+    else:
+        datasets.utils.logging.set_verbosity_error()
+        transformers.utils.logging.set_verbosity_error()
+
+    # Load data
+    raw_datasets = load_dataset("glue", args.task_name)
+
+    is_regression = args.task_name == "stsb"
+    if not is_regression:
+        label_list = raw_datasets["train"].features["label"].names
+        num_labels = len(label_list)
+    else:
+        num_labels = 1
+
+    # Load pretrained model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path, num_labels=num_labels)
+
+    # Preprocessing the datasets
+    sentence1_key, sentence2_key = task_to_keys[args.task_name]
+    padding = False if args.dynamic_length else "max_length"
+
+    def preprocess_function(examples):
+        # Tokenize the texts
+        texts = (
+            (examples[sentence1_key],) if sentence2_key is None else (examples[sentence1_key], examples[sentence2_key])
+        )
+        result = tokenizer(*texts, padding=padding, max_length=args.max_length, truncation=True)
+        result["labels"] = examples["label"]
+        return result
+
+    with accelerator.main_process_first():
+        processed_datasets = raw_datasets.map(
+            preprocess_function,
+            batched=True,
+            remove_columns=raw_datasets["train"].column_names,
+            desc="Running tokenizer on dataset",
+        )
+
+    train_dataset = processed_datasets["train"]
+    eval_dataset = processed_datasets["validation_matched" if args.task_name == "mnli" else "validation"]
+
+    # DataLoaders creation:
+    if not args.dynamic_length:
+        data_collator = default_data_collator
+    else:
+        data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
+
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, collate_fn=data_collator, batch_size=args.batch_size, drop_last=True
+    )
+    eval_dataloader = DataLoader(eval_dataset, collate_fn=data_collator, batch_size=args.batch_size, drop_last=not args.dynamic_length)
+
+    # Optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+
+    # Scheduler.
+    lr_scheduler = get_scheduler(
+        name="linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=len(train_dataloader) * args.num_epochs,
+    )
+
+    # Prepare everything with our `accelerator`.
+    model, optimizer, train_dataloader, eval_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, eval_dataloader, lr_scheduler
+    )
+
+    # Get the metric function
+    metric = evaluate.load("glue", args.task_name)
+
+    # Train!
+    # Only show the progress bar once on each machine.
+    train_steps = len(train_dataloader) * args.num_epochs
+    progress_bar = tqdm(range(train_steps), disable=not accelerator.is_local_main_process)
+    start_time = time.time()
+    for epoch in range(args.num_epochs):
+        model.train()
+        for step, batch in enumerate(train_dataloader):
+            # We need to skip steps until we reach the resumed step
+            outputs = model(**batch)
+            loss = outputs.loss
+            accelerator.backward(loss)
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+            if step == 0 and epoch == 0:
+                first_step_time = time.time() - start_time
+
+    total_training_time = time.time() - start_time
+    avg_iteration_time = (total_training_time - first_step_time) / (train_steps - 1)
+    print("Training finished.")
+    print(f"First iteration took: {first_step_time:.2f}s")
+    print(f"Average time after the first iteration: {avg_iteration_time * 1000:.2f}ms")
+
+    model.eval()
+    start_time = time.time()
+    for step, batch in enumerate(eval_dataloader):
+        with torch.no_grad():
+            outputs = model(**batch)
+        predictions = outputs.logits.argmax(dim=-1) if not is_regression else outputs.logits.squeeze()
+        predictions, references = accelerator.gather_for_metrics((predictions, batch["labels"]))
+        metric.add_batch(predictions=predictions, references=references)
+
+        if step == 0:
+            first_step_time = time.time() - start_time
+    total_eval_time = time.time() - start_time
+    avg_iteration_time = (total_eval_time - first_step_time) / (len(eval_dataloader) - 1)
+    print("Evaluation finished.")
+    print(f"First iteration took: {first_step_time:.2f}s")
+    print(f"Average time after the first iteration: {avg_iteration_time * 1000:.2f}ms")
+
+    eval_metric = metric.compute()
+    print(f"Result: {eval_metric}")
+
+if __name__ == "__main__":
+    main()
